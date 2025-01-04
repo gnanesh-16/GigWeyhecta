8bde8a98b03ada8932200bf1c7ab15d459d8fce4
2110040050sahil <2110040050@klh.edu.in>
2025-01-04 17:25:30 +0530

    add changes for the function

--- project/src/api.py
+++ project/src/api.py
@@ -1,11 +1,11 @@
 import requests
-
+from .server import recorder
 
 def main(req, res):
     response = requests.post('http://localhost:5173/start-recording')
-    
+    result = recorder.start_recording()
     return res.json({
-        'message': 'Recording started',
+        'message': result,
         'status_code': response.status_code,
         'response': response.json()
     })
